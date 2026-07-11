91981eadb28810de1cea034b3c66fa074ae85bed
Husain <hussainalmajed.2005@gmail.com>
2026-07-11 19:19:14 +0300

    created the .gitignore file

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+// creating the home page
+
+app.get('/' , async (req , res) => {
+
+    res.render('Home.ejs')
+
+})
 
 app.listen(3000 , () => {
     console.log('listining on port 3000!!!')
